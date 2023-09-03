1553a9dce8b2fa382e992f004b60e457e6f33bf9
Michael Isabella <michaelisabella1@gmail.com>
2023-09-02 20:10:47 -0400

    Abstracted the intro anim code for the about page

---
+++ scripts/aboutPageIntroAnim.js
@@ -0,0 +1,25 @@
+// Logo animation timeline
+const tl1 = anime.timeline({
+    easing: 'easeOutExpo',
+    duration: 500
+});
+
+tl1.add({
+targets: ['.aboutBackButton', '.toggleButton'],
+opacity: 1, 
+translateY: [5,0],
+delay: 800
+})      
+.add({
+targets: '#siteBio',
+opacity: 1,
+translateY: [5,0]
+})
+.add({
+targets: ['#photography', '#subjects', '#website', '#productionAssistant'], 
+opacity: 1,
+translateY: [5,0]
+})
+
+// Where all the action happens!
+tl1.play().finished.then(() => {})
